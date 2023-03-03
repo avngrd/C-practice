8147dcc8a854087ad8d3f25d87062e7657f83d19
Vadim <vadimlazuk33@gmail.com>
2023-03-03 13:29:05 +0300

    9th tasks

---
+++ Tasks_9/task_1/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace MyNamespace
+{
+    class Program
+    {
+        struct ZNAK
+        {
+            public string name, surname, zodiacSign;
+            public int[] birthday;
+        }
+        public static void Main()
+        {
+            ZNAK[] arr = new ZNAK[2];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Введите имя: ");
+                arr[i].name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите фамилию: ");
+                arr[i].surname = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите знак зодиака: ");
+                arr[i].zodiacSign = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите дату рождения: ");
+                arr[i].birthday = new int[3];
+                for (int j = 0; j < arr[i].birthday.Length; j++)
+                {
+                    arr[i].birthday[j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("ZNAK" + " " + (i + 1) + ":");
+                Console.WriteLine("Имя: " + arr[i].name);
+                Console.WriteLine("Фамилия: " + arr[i].surname);
+                Console.WriteLine("Знак зодиака: " + arr[i].zodiacSign);
+                for (int j = 0; j < arr[i].birthday.Length; j++)
+                {
+                    Console.Write(arr[i].birthday[j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("Введите фамилию человека:");
+            string inputSurname = Console.ReadLine();
+            personInformation(inputSurname, arr);
+        }
+        static bool isMin(ZNAK first, ZNAK second)
+        {
+            if (first.birthday[2] < second.birthday[2])
+                return false;
+            else if (first.birthday[2] > second.birthday[2])
+                return true;
+            else
+            if (first.birthday[1] < second.birthday[1])
+                return false;
+            else if (first.birthday[1] > second.birthday[1])
+                return true;
+            else
+            if (first.birthday[0] < second.birthday[0])
+                return false;
+            else if (first.birthday[0] > second.birthday[0])
+                return true;
+            else return false;
+        }
+        static void Sort(ZNAK[] arr)
+        {
+            ZNAK buf;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (isMin(arr[i], arr[j]))
+                    {
+                        buf = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = buf;
+                    }
+                }
+            }
+        }
+        static void personInformation(string inputSurname, ZNAK[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++) {
+                if (inputSurname == arr[i].surname) {
+                   Console.WriteLine("ZNAK" + " " + (i + 1) + ":");
+                   Console.WriteLine("Имя: " + arr[i].name);
+                   Console.WriteLine("Фамилия: " + arr[i].surname);
+                   Console.WriteLine("Знак зодиака: " + arr[i].zodiacSign);
+                    for (int j = 0; j < arr[i].birthday.Length; j++)
+                    {
+                        Console.Write(arr[i].birthday[j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Tasks_9/task_2/Program.cs
@@ -0,0 +1,95 @@
+﻿using static System.Console;
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Employees sergey = Employees.Sergey;
+            Employees vadim = Employees.Vadim;
+            Employees alex = Employees.Alex;
+            Employees artur = Employees.Artur;
+            Employees evegeniy = Employees.Evgeniy;
+            Employees maxim = Employees.Maxim;
+            Employees david = Employees.David;
+
+            string[] employeersNames = { "sergey", "vadim", "alex", "artur", "evgeniy", "maxim", "david" };
+            printAllEmployers(employeersNames);
+
+            Console.WriteLine("Введите имя сотрудника о котором хотите узнать информацию(для выходы из программы напишите exit): ");
+            string employeeChecker = Console.ReadLine();
+
+            switch(employeeChecker){
+                case "sergey":
+                    EmployeeInformation(sergey);
+                    break;
+                case "vadim":
+                    EmployeeInformation(vadim);
+                    break;
+                case "alex":
+                    EmployeeInformation(alex);
+                    break;
+                case "artur":
+                    EmployeeInformation(alex);
+                    break;
+                case "evgeniy":
+                    EmployeeInformation(evegeniy);
+                    break;
+                case "maxim":
+                    EmployeeInformation(maxim);
+                    break;
+                case "david":
+                    EmployeeInformation(david);
+                    break;
+                case "exit:":
+                    break;
+            }
+        }
+
+        enum Employees
+        {
+            Sergey = 10,
+            Vadim = 20,
+            Alex = 5,
+            Artur = 15,
+            Evgeniy = 19,
+            Maxim = 30,
+            David = 44,
+        }
+
+        static void EmployeeInformation(Employees employees)
+        {
+            switch (employees)
+            {
+                case Employees.Sergey:
+                    Console.WriteLine($"Сотруднику {Employees.Sergey} требуется отработать:{(int)Employees.Sergey} часов за месяц");
+                    break;
+                case Employees.Vadim:
+                    Console.WriteLine($"Сотруднику {Employees.Vadim} требуется отработать:{(int)Employees.Vadim} часов за месяц");
+                    break;
+                case Employees.Alex:
+                    Console.WriteLine($"Сотруднику {Employees.Alex} требуется отработать:{(int)Employees.Alex} часов за месяц");
+                    break;
+                case Employees.Artur:
+                    Console.WriteLine($"Сотруднику {Employees.Artur} требуется отработать:{(int)Employees.Artur} часов за месяц");
+                    break;
+                case Employees.Evgeniy:
+                    Console.WriteLine($"Сотруднику {Employees.Evgeniy} требуется отработать:{(int)Employees.Evgeniy} часов за месяц");
+                    break;
+                case Employees.Maxim:
+                    Console.WriteLine($"Сотруднику {Employees.Maxim} требуется отработать:{(int)Employees.Maxim} часов за месяц");
+                    break;
+                case Employees.David:
+                    Console.WriteLine($"Сотруднику {Employees.David} требуется отработать:{(int)Employees.David} часов за месяц");
+                    break;
+            }
+        }
+        static void printAllEmployers(string[] arr)
+        {
+            Console.WriteLine("Все сотрудники:");
+            for (int i = 0; i < arr.Length;i++) {
+            Console.WriteLine(i+"."+arr[i]);
+            }
+        }
+    }
+}
