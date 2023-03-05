a122c8db416f3ca2984186cfb4f81de715f6c51f
Vadim <vadimlazuk33@gmail.com>
2023-03-05 15:55:55 +0300

    some fixes

--- Tasks_2/task_1/Program.cs
+++ Tasks_2/task_1/Program.cs
@@ -8,6 +8,7 @@ namespace MyNamespace
             Console.WriteLine("Введите значение X:");
             double x = Convert.ToDouble(Console.ReadLine());
             double y;
+            double xqwe;
             double e = 2.7;
             if (x > 2) {
                 y = Math.Pow(3, x) * Math.Sqrt(x-2);
