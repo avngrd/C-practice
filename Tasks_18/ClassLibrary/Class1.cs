fe5dac04418904d1ffa64b99de12b905f24d4c9c
Vadim <vadimlazuk33@gmail.com>
2023-03-14 14:26:32 +0300

    added 18th tasks

---
+++ Tasks_18/ClassLibrary/Class1.cs
@@ -0,0 +1,116 @@
+﻿namespace ClassLibrary
+{
+    public class Triangle
+    {
+        public int sidea;
+        public int sideb;
+        public int sidec;
+
+        public Triangle(int sideA, int sideB, int sideC) { 
+            this.sidea = sideA;
+            this.sideb = sideB;
+            this.sidec = sideC;
+        }
+
+        public bool allSidesChecker() {
+            if (sidea == null) {
+                return false;
+            }else if (sideb == null)
+            {
+                return false;
+            }else if (sidec == null) {
+                return false;
+            }
+            return true;
+        }
+
+        public int PerimetrOfTriangle(int sideA, int sideB, int sideC)
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public double SquareOfTriangle(int sideA, int sideB, int sideC)
+        {
+            double perimetr = Convert.ToDouble(PerimetrOfTriangle(sideA,sideB, sideC));
+            double square = Convert.ToDouble(Math.Sqrt(perimetr * (perimetr - sideA) * (perimetr - sideB) * (perimetr - sideC)));
+            return square;
+        }
+
+        public string TypeOfTriangle(int sideA, int sideB, int sideC)
+        {
+            string triangleType = "";
+            if (sideA != sideB && sideA != sideC && sideB != sideC)
+            {
+                triangleType = "разносторонний";
+            }
+            else if (sideA == sideB && sideA == sideC)
+            {
+                triangleType = "равносторонний";
+            }
+            else if (sideA == sideB || sideA == sideC || sideB == sideC) {
+                triangleType = "равнобердренный";
+            }
+            return triangleType;
+        }
+    }
+
+    public class Rectangle
+    {
+        public int sidelength;
+        public int sidewidth;
+        
+
+        public Rectangle(int sideLength, int sideWidth)
+        {
+            this.sidelength = sideLength;
+            this.sidewidth = sideWidth;
+        }
+
+        public int PerimetrOfRectangle(int sideLength, int sideWidth)
+        {
+            return (sideLength + sideWidth) * 2;
+        }
+        public int SquareOfRectangle(int sideLength, int sideWidth)
+        {
+            return sideLength * sideWidth;
+        }
+    }
+
+    public class CarsPark
+    {
+        public string carname;
+        public string yearofrelease;
+        public double enginecapacity;
+        public decimal carprice;
+        public bool isonparking;
+
+        public CarsPark(string carName, string yearOfRelease, double engineCapacity, decimal carPrice, bool isOnParking)
+        {
+            this.carname = carName;
+            this.yearofrelease = yearOfRelease;
+            this.enginecapacity = engineCapacity;
+            this.carprice = carPrice;
+            this.isonparking = isOnParking;
+        }
+
+        public bool CheckParking(bool isOnParking)
+        {
+            if (!isOnParking)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void AddToParking()
+        {
+            isonparking = true;
+        }
+
+        public void RemoveFromParking()
+        {
+            isonparking = false;
+        }
+
+    }
+}
---
+++ Tasks_18/task_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using ClassLibrary;
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Создайте свою машину");
+            Console.WriteLine("Введите название марки: ");
+            string carName = Console.ReadLine();
+            Console.WriteLine("Введите год выпуска: ");
+            string yearOfRelease = Console.ReadLine();
+            Console.WriteLine("Введите обьем двигателя: ");
+            double engineCapacity = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите цену автомобиля: ");
+            decimal carPrice = Convert.ToDecimal(Console.ReadLine());
+            bool isOnParking = false;
+            CarsPark carsPark = new CarsPark(carName,yearOfRelease,engineCapacity,carPrice,isOnParking);
+
+            Console.WriteLine("Вы хотите узнать где ваш автомобиль 1 - да, 2 - нет");
+            int checkNumber = Convert.ToInt32(Console.ReadLine());
+            switch (checkNumber) {
+                case 1:
+                    bool whereCar = carsPark.CheckParking(isOnParking);
+                    if (whereCar)
+                    {
+                        Console.WriteLine("Ваш автомобиль на парковке");
+                        Console.WriteLine("Хотите убрать его с парковки 1 - да, 2 - нет");
+                        int needToRemoveCheck = Convert.ToInt32(Console.ReadLine());
+                        if (needToRemoveCheck == 1)
+                        {
+                            carsPark.RemoveFromParking();
+                            Console.WriteLine($"Автомобиль находится на парковке?{carsPark.isonparking}");
+                        }else if(needToRemoveCheck == 2)
+                        {
+                            Console.WriteLine("Автомобиль остается на парковке");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ваш автомобиль не на парковке");
+                        Console.WriteLine("Хотите поставить его на парковку 1 - да, 2 - нет");
+                        int needToAddCheck = Convert.ToInt32(Console.ReadLine());
+                        if (needToAddCheck == 1)
+                        {
+                            carsPark.AddToParking();
+                            Console.WriteLine($"Автомобиль находится на парковке?{carsPark.isonparking}");
+                        }else if(needToAddCheck == 2)
+                        {
+                            Console.WriteLine("Автомобиль остается на улице");
+                        }
+                    }
+                break;
+                case 2:
+                    Console.WriteLine("Выберите операцию: 1 - информация о автомобиле");
+                    int carOperation = Convert.ToInt32(Console.ReadLine());
+                    switch (carOperation)
+                    {
+                        case 1:
+                            Console.Write($"Марка автомобиля: {carsPark.carname}\n" +  $"Год выпуска: {carsPark.yearofrelease}\n" + $"Обьем двигателя:{carsPark.enginecapacity}\n" + $"Цена автомобиля: {carsPark.carprice}\n" + $"Ваш авто на парковке: {carsPark.isonparking}\n");
+                            break;
+
+                    }
+
+                    break;
+                default: Console.WriteLine("Неверный номер операции"); break;
+
+            }
+        }
+    }
+}
