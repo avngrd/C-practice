930a46cdeea858e25ecd1a53d3340ad9d1d66ba8
Vadim Lazuk <75860577+avngrd@users.noreply.github.com>
2023-04-06 22:47:19 +0300

    Update Program.cs

--- Tasks_1/task_1/Program.cs
+++ Tasks_1/task_1/Program.cs
@@ -8,7 +8,7 @@ namespace MyNamespace
             Console.WriteLine("Введите число а");
             Console.WriteLine("Введите число а");
             int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число b");
+            Console.WriteLine("Введите число b") ;
             int b = Convert.ToInt32(Console.ReadLine());
             int x = a / 2 + b / 2;
             Console.WriteLine(string.Format("{0:F3}", x));
